8b2eebe67d0aea53fd0d755f704ab21c1f6a80ae
Suyeong <hsy3539@naver.com>
2024-06-02 13:27:49 +0900

    strcmp 사용 X

--- Yeongjun/wasp_alarm_0602.m
+++ Yeongjun/wasp_alarm_0602.m
@@ -47,16 +47,16 @@ h = figure;
 
 while ishandle(h)
     %% 카메라에서 객체 인식
-    detected_object = get_detected_object(camera);
+    detected_object = get_detected_object(camera, detector);
 
     bboxes = detected_object.bboxes;
     labels = detected_object.labels;
-
+    
     %% 인식된 객체가 없다면 continue
     if isempty(bboxes)
         continue;
     end
-    
+        
     %% 말벌의 중심 좌표 획득
     centerXY = get_wasp_center(labels, bboxes);
 
--- webcam_detector.m
+++ webcam_detector.m
@@ -1,36 +1,32 @@
 clear camera
 %%
+
+% Load the detector
+detectorStruct = load('detector.mat');
+detector = detectorStruct.detector;  % Adjust this line based on the actual field name
+
 camera = webcam;
 h = figure;
 
 while ishandle(h)
-    I = snapshot(camera);
-    image(I)
+    %% 카메라에서 객체 인식
+    detected_object = get_detected_object(camera, detector);
+
+    bboxes = detected_object.bboxes;
+    labels = detected_object.labels;
+
+    %% 인식된 객체가 없다면 continue
+    if isempty(bboxes)
+        continue;
+    end
     
-    [bboxes,scores,labels] = detect(detector, I);
-    showShape("rectangle", bboxes, 'Label', labels)
-    drawnow
+    %% 말벌의 중심 좌표 획득
+    centerXY = get_wasp_center(labels, bboxes);
 
-    % 인식된 객체가 없으면 continue
-    if isempty(bboxes)
-        continue;
+    if ~isempty(centerXY)
+        disp('Detected wasp center coordinates:');
+        disp(centerXY);
     end
-
-    % 말벌 객체만 bboxes에 담음
-    bboxes = extract_wasp_bboxes(bboxes, labels);
-
-    % 말벌 객체가 없으면 continue
-    if isempty(bboxes)
-        continue;
-    end
-
-    for i = 2:size(bboxes, 1)
-        centerX = bboxes(i, 1) + bboxes(i, 3) / 2;
-        centerY = bboxes(i, 2) + bboxes(i, 4) / 2;
-        % 중심 좌표 출력
-        disp(['Box ', num2str(i), ': Center (X, Y) = (', num2str(centerX), ', ', num2str(centerY), ')']);
-    end
-
 end
 
 clear camera;
